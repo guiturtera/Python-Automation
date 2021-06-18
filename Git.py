61407a2b4186ee99d71759aeff540963f9e5c9fa
Guilherme <Turtera>
2021-06-18 15:05:46 -0300

    developing...

--- Git.py
+++ Git.py
@@ -1,21 +1,57 @@
 import re
+import io
 import os
 import git
-from git.repo.base import Repo
+from git.objects import commit
 
+class Commit:
+    def __init__(self, hash, message, name, date):
+        self.hash = hash
+        self.message = message
+        self.name = name
+        self.date = date
+        self.type, self.message = self.__get_message_info(message)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+    def __get_message_info(self, message):
+        data = re.split("[\(:]", message)[0]
+        return [ data[0], data[len(data) - 1] ]
+
+def get_available_commits():
+    with open("C:\\Users\\guilherme.turtera\\Desktop\\au\\.git\\hooks\Help\\available-commit-msg.txt", "r", encoding="utf-8") as file:
+        content = file.read().split('|')
+    return content
+
+def get_commit_type(message):
+    return re.split("[\(:]", message)[0]
+
+def get_last_release_date(g):
+    return g.log("-n 1", "--grep=release", "--oneline", "--pretty=%cs")
+
+def get_last_commits_log(g):
+    return g.log("--date=short", f"--since={get_last_release_date(g)}", "--oneline", "--pretty=%h|%s|%cn %ce|%cs")
+
+# get it by a relative path
 branch_path = os.path.abspath("..")
-print(branch_path)
 
-# hash, message, name
 g = git.Git(branch_path)
-raw_log_content = g.log("-n 5", "--oneline", "--pretty=%H|%s|%cn %ce")
-log_content = re.findall(".*\|.*\|.*", raw_log_content)
 
+commits_data = get_last_commits_log(g).split('\n')
 
-aux = tuple(re.split('\|', log_content[0]))
-aux1 = (2, 3, 4)
-for hash in aux1:
-    print(hash)
+#changelog_path = os.path.abspath("..\\changelog.md")
+#with open(changelog_path, "r", encoding="utf-8") as file:
+#    changelog_content = file.read()
 
-print(log_content)
+available_types = get_available_commits()
+dic = {}
+
+buf = io.StringIO()
+for log in commits_data:
+    commit_list = log.split('|') # hash|message|name lastname|short style date 
+    aux = Commit(commit_list[0], commit_list[1], commit_list[2], commit_list[3])
+    dic[aux.type].append(Commit(commit_list))
+
+print(buf.getvalue())
 
